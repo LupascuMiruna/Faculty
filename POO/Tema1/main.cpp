3fe4071c596ecfa722791970170ad247f63b6640
LupascuMiruna <mirunastefania2001@gmail.com>
2021-03-13 11:09:42 +0200

    set up

---
+++ POO/Tema1/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 13.03.2021.
+//
+
+#include "main.h"
---
+++ POO/Tema1/main.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 13.03.2021.
+//
+
+#ifndef TEMA1_MAIN_H
+#define TEMA1_MAIN_H
+
+class main {
+
+
+};
+
+
+#endif //TEMA1_MAIN_H
